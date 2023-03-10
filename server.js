79d212f65737733450a45bd61430babd1332ed88
Fathan <fathanshani.fs@gmail.com>
2023-03-11 06:36:48 +0700

    body-parser

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ import productRoutes from './routes/products.route.js'
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import sessionConfig from './config/session.config.js';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
@@ -15,8 +17,13 @@ app.use(cors({
     origin: "*" // allow any domain to make a request to this server (only in dev mode)
 }));
 app.get('/api/v1', (req, res) => res.send('API Ready'));
-app.use('/api/v1/routes', userRoutes)
+app.use('/users', userRoutes)
 app.use('/api/v1/products', productRoutes)
+
+// app.post('/login', (req, res) => {
+//     console.log({ requestFromOutside: req.body})
+//     res.send('login berhasil')
+// })
 
 
 app.listen(process.env.PORT || 3000, () => console.log(`server running on http://localhost:${process.env.PORT}`));
